9d685883bc6b18195e7907c651e6e839cf9c2456
Jaypen-git <54304688+Jaypen-git@users.noreply.github.com>
2023-03-28 16:42:19 -0400

    Add EX Folder via upload

---
+++ EX/InsectClient.java
@@ -0,0 +1,16 @@
+public class InsectClient {
+	public static void main(String[] args) {
+		Insect bug1 = new Insect(13, 31, 0);
+		System.out.println(bug1.getX());
+		bug1.setX(-314);
+		System.out.println(bug1.getX());
+		bug1.setX(133);
+		System.out.println(bug1.getX());
+		
+		Insect bug2 = new Insect(31);
+		System.out.println(bug2.getWeight());
+		System.out.println(bug2.getX());
+		System.out.println(bug2.getY());
+		System.out.println(Insect.getPopulation());
+	}
+}
